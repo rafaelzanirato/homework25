83116bab1c084d935f6e0badfa4c03e0dcdcca57
rafaelzanirato <84473402+rafaelzanirato@users.noreply.github.com>
2021-05-26 16:07:20 -0300

    Add files via upload

---
+++ fly_lion210524.py
@@ -0,0 +1,29 @@
+# Question 2
+name="rafael"
+# Question 3
+last="zanirato"
+# Question 4
+all=name+" "+last
+# Question 5
+print (all)
+# Question 6       
+print (all.title())
+# Question 7
+print (all.upper())
+# Question 8
+print ("rafael\nzanirato")
+# Question 9
+print (100-23)
+# Question 10
+print (97+12)
+# Question 11
+print (14*9)
+# Question 12
+print (112/18)
+# Question 13
+print (1+1)
+print ("1"+"1")
+print (str(1)+"1")
+print (1+int("1"))
+
+
